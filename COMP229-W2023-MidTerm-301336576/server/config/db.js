e4e46c3a3b406773d7e859a31ef5a3bf6c68eb1d
Mahfuz <>
2023-03-04 19:08:35 -0500

    first commit

---
+++ COMP229-W2023-MidTerm-301336576/server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+
+  "URI":"mongodb+srv://mahfuzur_rahman:mCeIID5gijdrKGvO@cluster0.vhzfvvy.mongodb.net/test"
+};
